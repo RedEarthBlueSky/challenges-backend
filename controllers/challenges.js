f073071a0758aa697176c89499313c8d05eefc32
Ian Salt <ian@isalt.digital>
2016-11-29 16:51:43 +0100

    feat:post challenge middleware

--- controllers/challenges.js
+++ controllers/challenges.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const passport = require('koa-passport');
 const uuid = require('uuid');
 const Challenge = require('../models').models.Challenge;
 const User = require('../models').models.User;
@@ -7,66 +8,37 @@ const User = require('../models').models.User;
 const axios = require("../lib/axios");
 
 exports.postChallenge = function* (next) {
-  const ctx = this;
-  let authToken = this.request.header['authorization']; // bearer token
-  authToken = authToken.split(' ');
-  if (authToken[0] === 'Bearer') authToken = authToken[1];
-  let challengeId = uuid.v4();
-  console.log(this.request.body.title);
-
-  yield User.findOne({ 'authToken': authToken })
-    .then((user)=>{
-      if (user !== null) {
-        let challengeDocument = {
-          challengeId: challengeId,
-          creatorUserId: user._id,
-          creatorName: user.profileInfo.firstName + ' ' + user.profileInfo.lastName,
-          created_at: + new Date(),
-          title:this.request.body.title,
-          comment:"Some comments not sure where from",
-          description:this.request.body.description,
-          totalChallenged: "0",
-          totalSubmitted: "0",
-          picture:this.request.body.imageURL
-        };
-        console.log(challengeDocument);
+  let ctx = this;
+  yield passport.authenticate('bearer', {session:false},
+  function *(err, user) {
+    if (user) {
+      let challengeId = uuid.v4();
+      let challengeDocument = {
+        challengeId: challengeId,
+        creatorUserId: user._id,
+        creatorName: user.profileInfo.firstName + ' ' + user.profileInfo.lastName,
+        created_at: + new Date(),
+        title:ctx.request.body.title,
+        comment:"Some comments not sure where from",
+        description:ctx.request.body.description,
+        totalChallenged: "0",
+        totalSubmitted: "0",
+        picture:ctx.request.body.imageURL
+      };
+      try {
         let newChallenge = new Challenge(challengeDocument);
-        console.log(newChallenge);
-        newChallenge.save();
-        console.log('New Challenge has been created!');
+        let temp = yield newChallenge.save();
         ctx.status = 200;
-        // ctx.body = newChallenge;
+        ctx.body = temp;
+      } catch(err) {
+        console.log(err);
       }
-    })
-    .catch((err) => {
-      console.log('Error getting user from bearer*:' + err);
-    });
-
-  //  Middleware, check there is a bearer token and retrieve user
-
-  //  Get user information from the database
-
-  //  ///////////////////////////////////////
-  //  Response 201 format
-  // {
-  //   *****  Create unique challenge ID
-  //   "id": "asdf90w8sifas0f820f",
-  //   *****  Get ID of user who created
-  //   "creatorUserId": "afsd772298rsuf",
-  //   *****  Give create timestamp
-  //   "created_at": "2015-08-05T08:40:51.620Z",
-  //   *****  Title from body of challenge
-  //   "title": "Cinnamon challenge",
-  //   *****  Description from body of challenge
-  //   "description": "Swallow a teaspoon of cinnamon if you can.",
-  //   *****  To be updated at a later date
-  //   "totalChallenged": "0",
-  //   *****  Count challenges in db and return submitted
-  //   "totalSubmited": "0",
-  //   *****  Image URL from body of submitted challenge
-  //   "imageURL": "http://image.slidesharecdn.com/deadlyviralvideotrends-160309201939/95/deadly-viral-video-trends-16-638.jpg?cb=1457606606",
-  //   "popularity":  "Rating system"
-  // }
+    }
+    else {
+      ctx.status = 401;
+      ctx.body = { error:  'Wrong token.'};
+    }
+  });
 };
 
 exports.getChallenges = function* (next) {
