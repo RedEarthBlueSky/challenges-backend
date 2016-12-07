5e9a05799aed3c4d4c0a70b7fdd77c8acf56a9e3
Ian Salt <ian@isalt.digital>
2016-12-07 12:55:21 +0100

    commit before merge

--- controllers/populate-db.js
+++ controllers/populate-db.js
@@ -4,20 +4,16 @@ const passport = require('koa-passport');
 const uuid = require('uuid');
 
 const User = require('../models').models.User;
-const userProfileInfo = require('./data.json');
+const Challenge = require('../models').models.Challenge;
+const populateData = require('./data.json');
 
-let userDocument = {
-  fbToken: '',
-  authToken: '',
-  profileInfo: {
-    fbId: '',
-    firstName: '',
-    lastName: '',
-    picture: '',  //  S3 URL
-    email: ''
-  }
-};
 
+//  grab the user document config from the data.json file
+let userDocument = populateData.userDocument;
+//  grab the challenge document config from the data.json file
+let challengeDocument = populateData.challengeDocument;
+
+//  declare function to create the new user document
 let createUserDocument = (data) => {
   userDocument.fbToken = uuid.v4();
   userDocument.authToken = uuid.v4();
@@ -29,13 +25,23 @@ let createUserDocument = (data) => {
   let newUser = new User(userDocument);
   console.log(newUser);
   newUser.save();
-  console.log('**************************');
-  console.log('**************************');
   console.log('New user has been created!');
 };
 
+//  declare functoin to create the new challenge document
+let createChallengeDocument = (data) => {
+  challengeDocument.challengeId = data.challengeId;
+  challengeDocument.creatorUserId = data.creatorUserId;
+  challengeDocument.created_at = new Date();
+  challengeDocument.title = data.title;
+  challengeDocument.description = data.description;
+  challengeDocument.totalChallenged = data.totalChallenged;
+  challengeDocument.totalSubmitted = data.totalSubmitted;
+  challengeDocument.imageURL = data.imageURL;
+};
+
 exports.populateDb = function* (next) {
-  let profileInfoArr = userProfileInfo.profileInfo;
+  let profileInfoArr = populateData.profileInfo;
   for (let i = 0; i < profileInfoArr.length; i++) {
     createUserDocument(profileInfoArr[i]);
   }
